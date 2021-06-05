c0c76207a910740712918e52114492604c0ea18c
narciSSuca <youngzelkind@mail.ru>
2021-06-06 02:55:34 +0300

    first CRUD on NestJS, God sorry

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,23 +1,35 @@
-import {Body, Controller, Get, Post, Param } from '@nestjs/common';
+import {Body, Controller, Delete, Get,
+        Post, Param, Put, Redirect,
+        Header, HttpCode, HttpStatus } from '@nestjs/common';
 import {CreateProductDto} from './dto/create-product.dto';
+import {UpdateProductDto} from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
 
   @Get()
-  index() {
+  index():string {
     return 'index qwery'
   }
 
   @Get(':id')
   show(@Param('id') id:string):string {
     return 'show ' + id
-    console.log('asd');
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   store(@Body() createProductDto: CreateProductDto):string {
     return `title: ${createProductDto.title} price:  ${createProductDto.price}`
   }
 
+  @Delete(':id')
+  destroy(@Param('id') id:string):string {
+    return `product ${id} remove`
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string):string {
+    return `product ${id} update`
+  }
 }
